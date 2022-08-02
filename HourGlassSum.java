c19e6682470f0f394c163f40b41bf0aabe2a5a3b
Rohit Singh Bhandari <105148888+Rohuu@users.noreply.github.com>
2022-08-02 16:42:46 +0530

    finding maximum hour glass sum in an 6*6 2d array.

--- HourGlassSum.java
+++ HourGlassSum.java
@@ -1,6 +1,5 @@
 public class HourGlassSum {
-    public static void main(String[] args);
-        
+    public static void main(String[] args); 
         int[][] arr = { { 1, 1, 1, 0, 0, 0 }, { 0, 1, 0, 0, 0, 0 }, { 1, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 },
                 { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }};
         int ans = maxHourGlassSum(arr);
