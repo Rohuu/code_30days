08fd3b757d47b674e78a54a0df8753eb492ca83f
Rohit Singh Bhandari <105148888+Rohuu@users.noreply.github.com>
2022-08-02 16:46:32 +0530

    palindrome programme for string.

--- IsPalindrome.java
+++ IsPalindrome.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
     String str= isPalindrome("aaaAAAaaa");
-        System.out.println(str);
+       System.out.println(str);
     }
     static String isPalindrome(String s){
         char[] ch=s.toCharArray();
